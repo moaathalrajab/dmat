5c93970247f00711da1e77f4b8a91b9e324250ce
Moaath Alrajab <moaath@alrajab.net>
2022-02-21 22:50:28 -0500

    Initial repo push for HW2

---
+++ src/main/java/com/mycompany/mavenproject1/Inherit.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author MoaathAlrajab
+ */
+
+   public class Inherit
+
+   { 
+
+ 
+
+              class Figure
+
+              {
+
+                      void display( )
+
+                      {
+
+                                 System.out.println("Figure");
+
+                      }
+
+              }
+
+ 
+
+              class Rectangle extends Figure
+
+              {
+
+                      void display( )
+
+                      {
+
+                                 System.out.println("Rectangle");
+
+                      } 
+
+              }
+
+ 
+
+              class Box extends Figure
+
+              {
+
+                      void display( )
+
+                      {
+
+                                 System.out.println("Box");
+
+                      }
+
+              } 
+
+ 
+
+              Inherit( )
+
+              {
+
+                      Figure f = new Figure( );
+
+                      Rectangle r = new Rectangle( );
+
+                      Box b = new Box( );
+
+                      f.display( );
+
+                      f = r;
+
+                      f.display( );
+
+                      f = b;
+
+                      f.display( );
+
+             } 
+
+ 
+
+              public static void main(String[ ] args)
+
+              {
+
+                      new Inherit( );
+
+              }
+
+} 
